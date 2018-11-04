3ed144a5fdd578969e985560ba9580b349c9f44e
FJEY <arcde40@gmail.com>
2018-11-04 16:28:47 +0900

    인코딩

---
+++ Source/src/FJEY/AST/ForExprNode.java
@@ -0,0 +1,25 @@
+package FJEY.AST;
+
+public class ForExprNode extends Node{
+	
+	/*Node left;
+	BinaryOpNode op;
+	SingleOpNode op1;
+	
+	public ForExprNode(Node left, BinaryOpNode op, SingleOpNode op1) {
+		super("ForExprNode", null);
+		this.left = left;
+		this.op = op;
+		if(left.getValue() instanceof Integer) {
+			for(int i = (int)left.getValue(); (boolean)op.getValue(); op1.val.val = op1.getValue()) {
+				AST.push(n);
+			}
+		}
+		
+	}
+	
+	public Object getValue() {
+		return left;
+	}*/
+	
+}
